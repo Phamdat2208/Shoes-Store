eb33c136138d19de4bd6d459e7078dc3b1f86a4b
datpt1 <datpt1@vnpay.vn>
2025-07-21 17:15:03 +0700

    shoes store

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,18 +3,29 @@ import { ProductListComponent } from '../product/product-list/product-list.compo
 import { ProductService } from '../../service/product.service';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductListComponent],
+  imports: [ProductListComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   private productService = inject(ProductService);
   public detailProduct!: any;
+  public allProduct!: any;
+
+  getNewProduct() {
+    this.productService.newProduct().subscribe((res: any) => this.detailProduct = res);
+  }
+
+  getAllProduct() {
+    this.productService.getAllProducts().subscribe((res: any) => this.allProduct = res)
+  }
 
   ngOnInit() {
-    this.productService.newProduct().subscribe((res: any) => this.detailProduct = res);
+    this.getNewProduct();
+    this.getAllProduct();
   }
 }
